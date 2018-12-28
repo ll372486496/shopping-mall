442c5429f141a5f39988488400bb9e2245117c32
ll372486496 <372486496@qq.com>
2018-12-28 17:22:17 +0800

    顶部搜索栏与底部tabbar

--- src/App.vue
+++ src/App.vue
@@ -4,36 +4,78 @@
     <div class="search-bar">
       <van-row>
           <van-col span="3">
-            <img :src='icon' class="icon" />
+            <img :src='icon' class="icon" width="80%"/>
           </van-col>
-          <van-col span="16">serach input</van-col>
-          <van-col span="5"><button><img src="" ></button> </van-col>
+          <van-col span="16">
+            <input type="text" class="search-input">
+          </van-col>
+          <van-col span="5">
+            <van-button size="mini">
+              查找
+            </van-button> 
+          </van-col>
       </van-row>
     </div>
     <router-view/>
+    <van-tabbar v-model="active" @change="changeTabbar(active)">
+            <van-tabbar-item icon="shop">首页</van-tabbar-item>
+            <van-tabbar-item icon="records">列表页</van-tabbar-item>
+            <van-tabbar-item icon="cart">购物车页</van-tabbar-item>
+            <van-tabbar-item icon="contact" info="20">会员中心</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      icon: require('../src/assets/images/money.png')
+      icon: require('../src/assets/images/money.png'),
+      active: 0
     }
+  },
+  methods: {
+    changeTabbar(active) {
+                console.log(active)
+                switch(active){
+                    case 0:
+                        this.$router.push({name:'ShoppingMall'})
+                        break;
+                    case 1:
+                        this.$router.push({name:'CategoryList'})
+                        break;
+                    case 2:
+                        this.$router.push({name:'Cart'})
+                        break;
+                    case 3:
+                        break;
+                }
+            }
   },
 }
 </script>
 
 <style>
   .search-bar{
-    width: 100%;
-    height: 2.2rem;
-    position: fixed;
-    top:10px;
-    line-height: 2.2rem;
+      height: 2.2rem;
+      background-color: #0066FF;
+      line-height:2.2rem;
   }
-  .search-bar img.icon{
-    width: 100%;
-    height: 100%;
-    
+  .search-input{
+      width:100%;
+      height: 1.3rem;
+      border-top:0px;
+      border-left:0px;
+      border-right:0px;
+      border-bottom: 1px solid 1px !important ;
+      background-color: #0066FF;
+      color:#fff;
+  }
+  .icon{
+      padding-top: .2rem !important;
+      padding-left: .3rem !important;
+  }
+  .vbtn{
+
+    border-radius: 50%!important;;
   }
 </style>
