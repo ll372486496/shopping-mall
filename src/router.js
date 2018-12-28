4634c4979c2a85fa4fe8f14fa3ed1efa677616f4
ll372486496 <372486496@qq.com>
2018-12-28 17:41:52 +0800

    修改路由词典，新建对应tabbar对应跳转组件

--- src/router.js
+++ src/router.js
@@ -1,20 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Cart from './components/pages/Cart'
+import Profile from './components/pages/Profile'
+import CategoryList from './components/pages/CategoryList'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/Home',
+      name: 'Home',
       component: Home
     },
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    } 
+      path: '/Cart',
+      name: 'Cart',
+      component: Cart
+    },
+    {
+      path: '/Profile',
+      name: 'Profile',
+      component: Profile
+    },
+    {
+      path: '/CategoryList',
+      name: 'CategoryList',
+      component: CategoryList
+    }
+
   ]
 })
