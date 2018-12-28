4ce66ef52efaf1bb75d3d75c706fe010f77c08c8
ll372486496 <372486496@qq.com>
2018-12-28 13:50:15 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <!-- 搜索栏 -->
+    <div class="search-bar">
+      <van-row>
+          <van-col span="3">
+            <img :src='icon' class="icon" />
+          </van-col>
+          <van-col span="16">serach input</van-col>
+          <van-col span="5"><button><img src="" ></button> </van-col>
+      </van-row>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      icon: require('../src/assets/images/money.png')
+    }
+  },
+}
+</script>
+
+<style>
+  .search-bar{
+    width: 100%;
+    height: 2.2rem;
+    position: fixed;
+    top:10px;
+    line-height: 2.2rem;
+  }
+  .search-bar img.icon{
+    width: 100%;
+    height: 100%;
+    
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+/* 引入vant,单独引入其css文件 */
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
