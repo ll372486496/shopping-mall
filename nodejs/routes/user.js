b9c5e251aa3b045197f6a55e4b8f3eebab144b2b
ll372486496 <372486496@qq.com>
2019-01-03 20:22:19 +0800

    服务器注册用户操作

--- nodejs/routes/user.js
+++ nodejs/routes/user.js
@@ -2,8 +2,26 @@ const express = require('express');
 const pool = require('../pool.js');
 var router = express.Router();
 router.post('/reg', function (req, res) {
-  console.log(req);
-  res.send('123');
+ var username = req.body.username;
+ var password = req.body.password;
+ var sql='SELECT * FROM mall_user WHERE uname=?';
+ pool.query(sql,[username],(err,result)=>{
+   if(err)throw err;
+   /* 当无结果时，可以插入 */
+   if(result.length<1){
+     var s="INSERT INTO mall_user VALUES (NULL, ?, ?,NOW())";
+     pool.query(s,[username,password],(err1,result1)=>{
+      if(err1)throw err1;
+      if(result1.affectedRows>0){
+       
+        res.send({code:1,msg:'注册成功'});
+      }
+     })
+    /* 有结果时，用户名已重复，返回错误提示 */
+   }else{
+    res.send({code:-2,msg:'用户名已存在'});
+   }
+ })
  
 })
 
