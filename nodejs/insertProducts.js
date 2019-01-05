8eea5cd2d0a20003f6117d8027e8a3c6a9c9e9e0
ll372486496 <372486496@qq.com>
2019-01-05 16:50:12 +0800

    从data中拿出数据插入数据库

---
+++ nodejs/insertProducts.js
@@ -0,0 +1,43 @@
+const fs=require('fs');
+const pool = require('./pool.js');
+/* 向数据库插入产品 */
+
+ /*  fs.readFile('data/goods.json','utf8',function(err, data){
+    let newData= JSON.parse(data)
+    let i=0
+    let pushData=[]
+    newData.RECORDS.map(function(value,index){
+        if(value.IMAGE1!=null){
+          let pid=value.ID
+          let pname=value. NAME
+          let subid=value. SUB_ID
+          let ori_price=value.ORI_PRICE
+          let pre_price=value.PRESENT_PRICE
+          let amount=value.AMOUNT
+          let detail=value.DETAIL
+          let sales=value.SALES_COUNT
+          let image1=value.IMAGE1
+          let ctime=value.CREATE_TIME
+          let utime=value.UPDATE_TIME
+          let pic=value.PICTURE_COMPERSS_PATH
+          let sql='INSERT INTO mall_products VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
+          pool.query(sql,[pid,pname,subid,ori_price,pre_price,amount,detail,sales,image1,ctime,utime,pic],function(err,res){})
+          
+        }  
+       
+    })
+  }); */
+/* 向数据库插入子分类数据 */
+fs.readFile('data/category_sub.json','utf8',function(err,data){
+  let newData= JSON.parse(data)
+  let i=0
+  let pushData=[]
+  newData.RECORDS.map(function(value,index){
+    let subid=value.ID;
+    let cid=value.MALL_CATEGORY_ID;
+    let sname=value.MALL_SUB_NAME;
+    let sql='INSERT INTO mall_categorysub VALUES (?,?,?)';
+    pool.query(sql,[subid,cid,sname],function(err,res){})
+  });
+})
+/* 向数据库插入子分类数据 */
