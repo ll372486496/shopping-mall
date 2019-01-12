d980ddf51b9ad3296d543dcbc482c62d0ddfe55c
ll372486496 <372486496@qq.com>
2019-01-12 16:56:04 +0800

    购物车组件

--- src/components/pages/Cart.vue
+++ src/components/pages/Cart.vue
@@ -1,15 +1,106 @@
 <template>
   <div>
-    购物车
+    <div>
+     <van-nav-bar   title="购物车" /> 
+    </div>
+    <van-checkbox-group class="card-goods" v-model="checkedGoods">
+      <van-checkbox
+        class="card-goods__item"
+        v-for="item in cartList"
+        v-model="item.isSelected"
+        :key="item.pid"
+        :name="item.pid"
+        
+      >
+        <van-card
+          :title="item.pname"
+         
+          :num="item.count"
+          :price="item.price"
+          :thumb="item.pic"
+        />
+      </van-checkbox>
+    </van-checkbox-group>
+    
+    <van-submit-bar
+     :price="totalPrice"
+     :button-text="submitBarText"
+     :disabled="!checkedGoods.length"
+    >
+      <van-checkbox v-model="allChecked">全选</van-checkbox>
+      
+    </van-submit-bar>
+
   </div>
 </template>
 
 <script>
   export default {
-    
+    created(){
+      this.getCartItem()
+      console.log(this.checkedGoods);
+    },
+    data(){
+      return{
+        cartList:[],
+        isEmpty:false,
+        uid:1,
+        checkedGoods:[],
+        allChecked:false,
+      }
+    },
+    methods:{
+      getCartItem(){
+        var url='http://127.0.0.1:3000/cartItem?uid='+this.uid;
+        this.axios.get(url).then(res=>{
+          if(res.data.code<0){
+            this.isEmpty=true;
+          }else{
+            this.isEmpty=false;
+            console.log(res.data.data);
+            this.cartList=res.data.data;
+            for(var item of this.cartList){
+              item.isSelected=true;
+             
+            }
+            console.log(this.cartList);
+          }
+          
+        })
+      },
+     
+     
+    },
+    computed:{
+      totalPrice(){
+        /* console.log(this.cartList.length)
+        console.log(this.checkedGoods.length) */
+        var total=0;
+        for(var item of this.cartList){
+          if(this.checkedGoods.indexOf(item.pid)!=-1){
+
+          /*   console.log(item); */
+            total+=Number(item.price)*item.count;
+          }
+        }
+       /*  console.log(total) */
+        return total*100;
+        
+      },
+      submitBarText(){
+        return `结算(${this.checkedGoods.length})`
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+  .card-goods {
+  padding: 10px 0;
+  background-color: #fff;
+  }
+  .card-goods__item{
+     position: relative;
+    background-color: #fafafa;
+  }
 </style>
